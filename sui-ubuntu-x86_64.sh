06d1ebc2d033c6f8e21e9414332a5c5c8537059f
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2023-12-26 12:27:36 +0000

    Update sui-ubuntu-x86_64.sh script to support specifying release version

--- sui-ubuntu-x86_64.sh
+++ sui-ubuntu-x86_64.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-RELEASE="mainnet-v1.15.1"
+# 使用参数指定的版本，如果未提供，则默认为 "mainnet-v1.15.1"
+RELEASE=${1:-"mainnet-v1.15.1"}
+FILENAME="sui-${RELEASE}-ubuntu-x86_64.tgz"
 
 sudo apt-get update
 sudo apt-get install -y curl tar
 
-curl -L https://github.com/MystenLabs/sui/releases/download/${RELEASE}/sui-${RELEASE}-ubuntu-x86_64.tgz | tar xz
+if [ -f "$FILENAME" ]; then
+    echo "使用已下载的文件 $FILENAME"
+    tar xz -f $FILENAME
+else
+    echo "Download $FILENAME"
+    if ! curl -L "https://github.com/MystenLabs/sui/releases/download/${RELEASE}/${FILENAME}" | tar xz; then
+        echo "下载失败，请检查网络连接后重试或手动下载：https://github.com/MystenLabs/sui/releases/"
+        echo "下载完成后，请将压缩包放置在脚本同级目录带上版本号重新运行脚本 .eg: ./sui-ubuntu-x86_64.sh mainnet-v1.15.1"
+        exit 1
+    fi
+fi
 
-sudo mkdir /sui
+sudo mkdir -p /sui
 sudo mv ./target/release/sui-faucet-ubuntu-x86_64 /sui/sui-faucet
 sudo mv ./target/release/sui-node-ubuntu-x86_64 /sui/sui-node
 sudo mv ./target/release/sui-test-validator-ubuntu-x86_64 /sui/sui-test-validator
 sudo mv ./target/release/sui-tool-ubuntu-x86_64 /sui/sui-tool
 sudo mv ./target/release/sui-ubuntu-x86_64 /sui/sui
 
-# 配置环境变量
 echo 'export PATH="/sui:$PATH"' >> $HOME/.bashrc
 
 echo "Sui installation completed. Please run 'source $HOME/.bashrc' to update the PATH."
